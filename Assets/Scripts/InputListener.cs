e3874562a099d29d7ee2ab02ab7e4346a629f775
Jonathan Chien <jonathan.chien@gmail.com>
2019-01-26 23:54:48 -0800

    Can grab object tagged "Interactable". Must have a collider and rigidbody components.

--- Assets/Scripts/InputListener.cs
+++ Assets/Scripts/InputListener.cs
@@ -7,6 +7,10 @@ public class InputListener : MonoBehaviour
     public Text debugText;
     public GameObject controller;
     public GameObject controllerRay;
+    private GameObject _grabbed;
+    private Vector3 _relativeOffset;
+    private Quaternion _startParentRotation;
+    private Quaternion _startChildRotation;
 
     public void Start()
     {
@@ -24,14 +28,23 @@ public class InputListener : MonoBehaviour
         // Controller raycast.
         Ray ray = new Ray(controller.transform.position, controller.transform.forward);
         RaycastHit hit;
-        GameObject target;
+        GameObject target = null;
         string targetName;
-        float width = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) ? 2 : 1;
+        bool triggerDown = OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger);
+        float width = triggerDown ? 2 : 1;
         if (Physics.Raycast(ray, out hit, 1000f))
         {
             controllerRay.transform.localScale = new Vector3(width, width, hit.distance);
             target = hit.collider.gameObject;
             targetName = target.name;
+            if (_grabbed == null && target.tag == "Interactable" && triggerDown)
+            {
+                _grabbed = target;
+                _relativeOffset = controller.transform.InverseTransformPoint(_grabbed.transform.position);
+                _startParentRotation = controller.transform.rotation;
+                _startChildRotation = _grabbed.transform.rotation;
+                _grabbed.GetComponent<Rigidbody>().useGravity = false;
+            }
         }
         else
         {
@@ -39,10 +52,29 @@ public class InputListener : MonoBehaviour
             targetName = "None";
         }
 
+        if (_grabbed != null && !triggerDown)
+        {
+            _grabbed.GetComponent<Rigidbody>().useGravity = true;
+            _grabbed = null;
+        }
+
+        if (_grabbed != null)
+        {
+            _grabbed.transform.position = controller.transform.TransformPoint(_relativeOffset);
+            _grabbed.transform.rotation = controller.transform.rotation * Quaternion.Inverse(_startParentRotation) * _startChildRotation;
+        }
+
         if (debug)
         {
             Vector2 axis = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
-            debugText.text = $"localPos={localPos}\nlocalRot={localRot}\ntarget={targetName}\naxis={axis}";
+            string tag = "None";
+            if (target != null)
+            {
+                tag = target.tag;
+            }
+
+            string grabbedName = _grabbed == null ? "None" : _grabbed.name;
+            debugText.text = $"localPos={localPos}\nlocalRot={localRot}\ntarget={targetName}\ntag={tag}\ngrabbed={grabbedName}\naxis={axis}\ntriggerDown={triggerDown}";
         }
     }
 }
